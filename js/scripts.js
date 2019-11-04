721a3d583de6d2b80ea722c92c2ed77e37c8ea0f
Student <example@example.com>
2019-11-04 15:14:53 -0800

    not function completely, moving on to next class assignment

--- js/scripts.js
+++ js/scripts.js
@@ -3,24 +3,28 @@ function VacationPlaces () {
   this.destinations = []
 }
 function Destinations (location, landmarks, season) {
-  this.location = location,
-  this.landmarks = landmarks,
-  this.season = season
+  this.location = location;
+  this.landmarks = landmarks;
+  this.season = season;
 }
 
 Destinations.prototype.total = function() {
   return this.location + this.landmarks + this.season;
 }
 
-var lincolnCity = new Destinations ("Oregon Coast", ["Devil's Lake", "D river beach"], "Summer");
-var Seattle = new Destinations ("On the bay in Washington", ["Space Needle", "Experience Music Project"], "Fall");
-var Paris = new Destinations ("France", ["The Louvre", "The Seine river"], "Winter");
+var lincolnCity = new Destinations ("Oregon Coast, ", ["Devil's Lake", " D river beach, "], "Summer");
+var seattle = new Destinations ("On the bay in Washington, ", ["Space Needle", "Experience Music Project"], " Fall");
+var paris = new Destinations ("France ", ["The Louvre", " The Seine river "], "Winter");
 
 // User Interface Logic ---------
 $(document).ready(function(){
-  $("#places").click(function(event) {
-  event.preventDefault()
+  $(".places").on("click" , function(event) {
+    this.classList.toggle("lincolnCity")
+  event.preventDefault();
 
-// ("#lincolnCity")(lincolnCity.total).text();
+$(".lincolnCity").text(lincolnCity.total());
+$(".seattle").text(seattle.total());
+$(".paris").text(paris.total());
+
 })
 });
